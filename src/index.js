3dc8243be21a0780955d231adca9b4694a844809
Richard Mohammed <richard@oneabsolute.co.uk>
2020-12-09 20:46:08 +0000

    Templates updated with some base code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const expressNunjucks = require("express-nunjucks");
+const isDev = app.get("env") === "development" || true;
+
+app.set("views", __dirname + "/../" + "templates");
+
+const njk = expressNunjucks(app, {
+  watch: isDev,
+  noCache: isDev,
+  autoescape: true,
+});
 
 app.get("/", (req, res) => {
-  res.send("Its working");
+  res.render("home", { roomId: req.query.id });
+});
+
+app.get("/status", (req, res) => {
+  res.render("status", { port: port });
 });
 
 app.listen(port, () => {
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,8 @@
 const chai = require("chai");
 const app = require("../src/index");
 const chaiHttp = require("chai-http");
-expect = chai.expect;
+const expect = chai.expect;
+const should = chai.should();
 chai.use(chaiHttp);
 
 describe("Express server", () => {
@@ -12,6 +13,30 @@ describe("Express server", () => {
         .get("/")
         .end((err, res) => {
           expect(res).to.have.status(200);
+          done();
+        });
+    });
+  });
+
+  describe("/status", () => {
+    it("Responds with status 200", (done) => {
+      chai
+        .request(app)
+        .get("/status")
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+
+    it("Serves status contents from template", (done) => {
+      chai
+        .request(app)
+        .get("/status")
+        .end((err, res) => {
+          console.log("res", res.text);
+
+          res.text.should.match(/<h1>Server running on port 8080<\/h1>/);
           done();
         });
     });
